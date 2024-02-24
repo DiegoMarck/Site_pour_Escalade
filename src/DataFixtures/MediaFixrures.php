265cc71391f158f5159fd6759920f1cfad2dc0d5
Diego de Massy <ddemassy@gmail.com>
2024-02-24 19:39:22 +0100

    2ième changement mediafixtures
    copie code GTP

--- src/DataFixtures/MediaFixrures.php
+++ src/DataFixtures/MediaFixrures.php
@@ -1,13 +1,34 @@
 <?php
 
+// namespace App\DataFixtures;
+
+// use Faker\Factory;
+// use App\Entity\Media;
+// use App\DataFixtures;
+// use Doctrine\Persistence\ObjectManager;
+// use Doctrine\Bundle\FixturesBundle\Fixture;
+// use Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInterface;
+
+// class MediaFixtures extends Fixture
+// {
+//     public function load(ObjectManager $manager)
+//     {
+//         $media = new Media();
+//         $media->setNom('Nom de mon image');
+//         $media->setDescription('Une description');
+       
+
+//         $manager->persist($media);
+//         $manager->flush();
+//     }
+        
+// }
+    
 namespace App\DataFixtures;
 
-use Faker\Factory;
 use App\Entity\Media;
-use App\DataFixtures;
-use Doctrine\Persistence\ObjectManager;
 use Doctrine\Bundle\FixturesBundle\Fixture;
-use Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInterface;
+use Doctrine\Persistence\ObjectManager;
 
 class MediaFixtures extends Fixture
 {
@@ -21,6 +42,4 @@ class MediaFixtures extends Fixture
         $manager->persist($media);
         $manager->flush();
     }
-        
 }
-    
